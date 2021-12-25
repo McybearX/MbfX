c51dfdb2f7ed4de2b8b4f0bfd1bbebe70374f600
Mcybear× <95560890+McybearX@users.noreply.github.com>
2021-12-25 22:06:19 +0700

    Add files via upload

---
+++ MbfX.py
@@ -0,0 +1,10 @@
+import os,time
+
+os.system("clear")
+
+os.system("figlet NOTE")
+print ("\x1b[1;91mMOHON MAAF SCRIPT INI SUDAH TIDAK BISA DIGUNAKAN 🙏")
+print ("\x1b[1;92mTUNGGU UPDATE TOOLS SELANJUTNYA DI YOUTUBE KAMI👍")
+print ("\x1b[1;97mlink youtube : https://youtube.com/c/MBEWLEGS")
+time.sleep(7)
+os.system("xdg-open https://youtube.com/c/MBEWLEGS")
